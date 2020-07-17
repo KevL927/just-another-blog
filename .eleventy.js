880924cc4c3e0a4bffe0dfbf0da8dc738cd7e80f
Kevin Lee <ki.lee@sba.gov>
2020-07-17 00:39:13 -0400

    Update time zone

--- .eleventy.js
+++ .eleventy.js
@@ -9,14 +9,14 @@ module.exports = function(eleventyConfig) {
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
       return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
+        zone: 'local'
       }).toFormat('yy-MM-dd');
     });
 
     eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat("dd-MM-yy");
+      zone: 'local'
+    }).toFormat("mm-dd-yy");
   });
 
   return {
